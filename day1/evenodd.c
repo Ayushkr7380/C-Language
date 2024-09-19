494af6b5c000d39cc4cb5bb30a3e35727e567aa6
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-19 19:36:33 +0530

    evenodd

---
+++ day1/evenodd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    if (a%2 == 0)
+    {
+        printf("%d is even number.",&a);
+    }
+    else
+    {
+        printf("%d is odd number.",a);
+    }    
+    return 0;
+}
