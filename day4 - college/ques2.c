fbc17be2821a51e8bb3ac96096e369d33ec0c530
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-23 19:35:58 +0530

    code added

--- day4 - college/ques2.c
+++ day4 - college/ques2.c
@@ -1,5 +1,4 @@
 //Print the following Patters using only printf function.
-
 #include<stdio.h>
 int main(){
     int i,j;
@@ -23,25 +22,35 @@ int main(){
     //     printf("\n");
     // }
 
-     for ( i = 2; i >=0; i--)
-    {
-        for (j = 2; j >=0; j--)
-        {
-            if (i==1 && j ==1)
-            {
+    //  for ( i = 2; i >=0; i--)
+    // {
+    //     for (j = 2; j >=0; j--)
+    //     {
+    //         if (i==1 && j ==1)
+    //         {
+    //             printf(" ");
+    //         }
+    //         else
+    //         {
+    //             printf("*");
+                
+    //         }
+            
+    //     }
+
+    //     printf("\n");
+    // }
+
+    for(i=0;i<=2;i++){
+        for(j=0;j<=2;j++){
+            if((i==1 && j==0) || (i==1 && j==2) || (i==2 && j==0) || (i==2 && j==2)){
                 printf(" ");
             }
-            else
-            {
+            else{
                 printf("*");
-                
             }
-            
         }
-
         printf("\n");
     }
-
-
     return 0;
 }
