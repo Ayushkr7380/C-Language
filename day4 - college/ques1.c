768361a387c675f6a33da546d5a977e2fab8ee58
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-23 10:35:59 +0530

    code added

---
+++ day4 - college/ques1.c
@@ -0,0 +1,16 @@
+//Write a C program to print the following details along with the given prompt message.
+//Prompt -- Good Morning My name is Ayush.
+//Details -- Print any 5 lines about you.
+
+#include<stdio.h>
+int main(){
+    //Prompt
+    printf("Good Morning ,My name is Ayush");
+    //Details
+    printf("\n1. I am from Muzaffarpur,Bihar");
+    printf("\n2. I am 22 year Old.");
+    printf("\n3. I have pursued BCA from LNMCBM College.");
+    printf("\n4. I am Pursuing MCA from GNIOT College.");
+    printf("\n5. I am currently using VS Code.");
+    return 0;
+}
