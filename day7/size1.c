500de16ae51533a87f2feffe1f9b998c0f4204db
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-27 23:47:17 +0530

    code added

--- day7/size1.c
+++ day7/size1.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main(){
     printf("Size of Int is %u bytes\n",sizeof(int));
+    printf("Size of Float is %u bytes\n",sizeof(float));
+    printf("Size of Float is %u bytes\n",sizeof(char));
+    printf("Size of Float is %u bytes\n",sizeof(double));
     return 0;
 }
