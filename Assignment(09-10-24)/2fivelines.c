a71ada9486640459fd50912e87b2c8cdafd20420
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-09 22:13:30 +0530

    code added

---
+++ Assignment(09-10-24)/2fivelines.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("I am Ayush kumar.\n");
+    printf("I have pursued BCA from LNMCBM.\n");
+    printf("Currently I am purshuing MCA from GNIOT.\n");
+    printf("I am currently learning C Language.\n");
+    printf("I want to become a Full Stack Webdeveloper.\n");
+    return 0;
+}
