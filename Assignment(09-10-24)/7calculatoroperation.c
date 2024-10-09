79f107ed1d1fd8e8cbf1e665366a887d9e9bc0f6
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-09 23:24:25 +0530

    comment added

---
+++ Assignment(09-10-24)/7calculatoroperation.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int num1 = 10 , num2 = 2; //Declare and Initialize value in num1 and num2 varaible.
+    int result; //Decalre varaible result to store the sum / difference / multiplication / quotient operation of num1 and num2.
+
+    result = num1 + num2; //Sum of two num1 and num2
+    printf("sum of %d and %d is %d\n",num1,num2,result); //Print the sum result.
+
+    result = num1 - num2; //Subtration of two num1 and num2
+    printf("Subtration of %d and %d is %d\n",num1,num2,result); //Print the Subtration result.
+
+    result = num1 * num2; //Multiplication of two num1 and num2
+    printf("Multiplication of %d and %d is %d\n",num1,num2,result); //Print the Multiplication result.
+
+    result = num1 / num2; //Quotient of two num1 and num2
+    printf("Quotient of %d and %d is %d\n",num1,num2,result); //Print the Quotient result.
+
+    return 0; //return 0 tells the successfully execution of program.
+}
