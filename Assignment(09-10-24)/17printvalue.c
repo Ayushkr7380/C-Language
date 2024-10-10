48eb30532a214a6b529b028be7f291401cd0c1e0
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-10 21:33:57 +0530

    code added

---
+++ Assignment(09-10-24)/17printvalue.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1; //declare varaible num1 to store user's input value.
+
+    printf("Enter any number : "); //prompt user to enter any number.
+    scanf("%d",&num1); //read and store user's input in num1.
+
+    printf("%d",num1); //print the value of num1 in terminal.
+    return 0;//return 0 tells the successfull execution of the program.
+}
