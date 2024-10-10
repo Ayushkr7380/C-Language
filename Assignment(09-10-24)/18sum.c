e2841bd5058a4694ca60d9dae3f973e637824bde
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-10 21:46:02 +0530

    code added

---
+++ Assignment(09-10-24)/18sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int num1,num2; //declare varaible num1 and num2 to store user's input value.
+    int sum; //declare varaible num to store the sum of num1 and num2.
+
+    printf("Enter 1st number : "); //prompt user to enter 1st number.
+    scanf("%d",&num1); //read and store user's input in num1.
+
+    printf("Enter 2nd number : "); //prompt user to enter 2nd number.
+    scanf("%d",&num2); //read and store user's input in num2.
+
+    sum = num1 + num2; //store the sum of num1 and num2 in sum varaible.
+
+    printf("sum of %d and %d is %d",num1,num2,sum); //print the sum of num1 and num2 in terminal. 
+    return 0; //return 0 tells the successfull execution of the program.
+}
