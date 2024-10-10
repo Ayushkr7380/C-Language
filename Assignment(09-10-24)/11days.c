edcb953d7485d2cfdedb21ecfa6710f3149a0f8e
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-10 20:12:02 +0530

    code added

---
+++ Assignment(09-10-24)/11days.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int num; //declare varaible num to store user's input value.
+
+    printf("Enter any number between 1 to 7 : "); //prompt user to enter any number.
+    scanf("%d",&num); //read and store user's input value.
+
+    switch(num){
+        case 1:printf("Sunday."); //print sunday in terminal when user input number is 1.
+            break;
+        case 2:printf("Monday."); //print monday in terminal when user input number is 2.
+            break;
+        case 3:printf("Tuesday."); //print tuesday in terminal when user input number is 3.
+            break;
+        case 4:printf("Wednesday."); //print wednesday in terminal when user input number is 4.
+            break;
+        case 5:printf("Thursday."); //print thursday in terminal when user input number is 5.
+            break;
+        case 6:printf("Friday."); //print friday in terminal when user input number is 6.
+            break;
+        case 7:printf("Saturday."); //print saturday in terminal when user input number is 7.
+            break;
+        default:printf("Enter Number between 1 to 7 only.");
+    }
+    return 0; //return 0 tells the successfull execution of the program.
+}
