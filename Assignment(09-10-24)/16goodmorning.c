b64a5657122a47efb98810bcd4d2429d9aa7ccbc
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-10 21:25:07 +0530

    code added

---
+++ Assignment(09-10-24)/16goodmorning.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("Good Morning All.\n"); //Prompt Good Morning All message in terminal.
+
+    //Write 5 lines about yourself.
+    printf("This is about Myself.\n");
+    printf("I am ayush kumar.\n");
+    printf("I have pursued BCA.\n");
+    printf("I am currently pursuing MCA.\n");
+    printf("I am from Bihar.\n");
+    printf("I want to become a full stack web developer.\n");
+    return 0; //return 0 tells the successfull execution of the program.
+}
