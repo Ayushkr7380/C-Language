22b2b3928ca5f9e56b317445dfa4140e154030c0
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-10 20:31:49 +0530

    code added

---
+++ Assignment(09-10-24)/12greatestofthree.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int num1 , num2 , num3; //declare varaible num1 ,num2 and num3 to store user's input value.
+
+    printf("Enter 1st number : "); //prompt user to enter 1st number.
+    scanf("%d",&num1); //read and store user's input in num1.
+
+    printf("Enter 2nd number : "); //prompt user to enter 2nd number.
+    scanf("%d",&num2); //read and store user's input in num2.
+
+    printf("Enter 3rd number : "); //prompt user to enter 3rd number.
+    scanf("%d",&num3); //read and store user's input in num3.
+
+    //Check num1 is greater than num2 and num3
+    if( num1 >= num2 && num1 >= num3){
+        printf("%d is the greatest number.",num1);
+    }
+    //check num2 is greater than num1 and num3
+    else if( num2 >= num1 && num2 >= num3){
+        printf("%d is the greatest number.",num2);
+    }
+    //When num1 or num2 is not greater then else will print num3 as greatest.
+    else{
+        printf("%d is the greatest number.",num3);
+    }
+    return 0; //return 0 tells the successfull execution of the program.
+}
