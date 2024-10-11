2d86f8ded47d28afbe0f7777c5eb38390f327cbb
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-11 11:39:49 +0530

    code added

---
+++ Assignment(09-10-24)/22table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num1; //declare varaible num1 to store user's input value.
+
+    printf("Enter any number : "); //prompt user to enter any number.
+    scanf("%d",&num1); //read and store user's input value in num1.
+
+    //Create a for loop to print table of user's input number form 1 to 10.
+    for(int i=1;i<=10;i++){
+        printf("%d x %d = %d\n",num1,i,(num1*i));
+    }
+    return 0; //return 0 tells the successfull execution of the program.
+}
