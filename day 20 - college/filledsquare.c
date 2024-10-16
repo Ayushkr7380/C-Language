e9a2ec8f749b16545d1237d5ca59c1321237ecbb
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-10-16 20:56:32 +0530

    code added

---
+++ day 20 - college/filledsquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=0;i<=2;i++){
+        for(j=0;j<=2;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
