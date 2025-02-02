9850fcde4bc6fe16c1121c6186e6eb5b69c1a5e2
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-02-02 18:41:11 +0530

    code added

--- Exam-Prep/armStrong.c
+++ Exam-Prep/armStrong.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int num;
+    int num,sum=0;
     printf("Enter any number : ");
     scanf("%d",&num);
 
@@ -11,6 +11,25 @@ int main(){
         lengthOFNum++;
     }
 
-    printf("Length of number is %d.",lengthOFNum);
+    temp = num;
+    while(temp !=0){
+        int rem = temp % 10;
+        int power = 1;
+        for (int i = 0; i < lengthOFNum; i++) {
+            power *= rem;
+        }
+        sum+=power;
+        temp /= 10;
+
+    }
+    
+    printf("Sum is %d\n",sum);
+
+    if(num == sum){
+        printf("%d is a armstrong number.",num);
+    }
+    else{
+        printf("%d is not a armstrong number.",num);
+    }
     return 0;
 }
