a7c960c8236d23a1e0fec55d8fa412412e61063d
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-03-02 00:56:26 +0530

    Decimaltobinary code updated

--- Exam-Prep/DecimalToBinary.c
+++ Exam-Prep/DecimalToBinary.c
@@ -22,7 +22,7 @@ void decimalToBinary(int num) {
 
 int main() {
     int num;
-    printf("Enter a decimal number: ");
+    printf("Enter a decimal number : ");
     scanf("%d", &num);
     
     printf("Binary of %d is : ",num);
