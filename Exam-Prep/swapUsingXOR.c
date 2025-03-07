8c0bf8b80c7c718d1b3d0d460d6bc51fbaf65469
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-03-07 23:26:58 +0530

    comment added

--- Exam-Prep/swapUsingXOR.c
+++ Exam-Prep/swapUsingXOR.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main(){
     int num1 = 10,num2 = 20;
+
+    //Display before swapping
     printf("Before swapping..\n");
     printf("num1 - %d || num2 - %d\n",num1,num2);
 
@@ -9,6 +11,7 @@ int main(){
     num2 = num1 ^ num2;
     num1 = num1 ^ num2;
 
+    //Display after swapping
     printf("After swapping..\n");
     printf("num1 - %d || num2 - %d\n",num1,num2);
     return 0;
